c257702b7914a56078d582f92be046d4166906cf
Alireza2794 <alirezahaghshenas12@gmail.com>
2025-03-02 20:39:08 +0330

    change

--- app.py
+++ app.py
@@ -86,6 +86,4 @@ def solve_math():
     return jsonify({'results': results})
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))  # گرفتن پورت از متغیر محیطی
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True)
